7d5014ea6636fef3ddc31a671fd9329f7a48a1d0
Vikas Lagerweij <83780520+RealzPrestige@users.noreply.github.com>
2023-01-22 17:58:15 +0100

    fixes

--- src/main/java/dev/zprestige/mud/module/combat/AutoBed.java
+++ src/main/java/dev/zprestige/mud/module/combat/AutoBed.java
@@ -3,15 +3,9 @@ package dev.zprestige.mud.module.combat;
 import dev.zprestige.mud.Mud;
 import dev.zprestige.mud.events.bus.EventListener;
 import dev.zprestige.mud.events.impl.player.MotionUpdateEvent;
-import dev.zprestige.mud.events.impl.render.Render2DEvent;
-import dev.zprestige.mud.events.impl.render.Render3DEvent;
 import dev.zprestige.mud.module.Module;
 import dev.zprestige.mud.setting.impl.BooleanSetting;
-import dev.zprestige.mud.setting.impl.ColorSetting;
-import dev.zprestige.mud.setting.impl.FloatSetting;
 import dev.zprestige.mud.setting.impl.IntSetting;
-import dev.zprestige.mud.shader.impl.BufferGroup;
-import dev.zprestige.mud.shader.impl.GlowShader;
 import dev.zprestige.mud.util.impl.*;
 import net.minecraft.block.Block;
 import net.minecraft.client.gui.inventory.GuiContainer;
@@ -32,7 +26,6 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.RayTraceResult;
 import net.minecraft.util.math.Vec3i;
 
-import java.awt.*;
 import java.util.stream.IntStream;
 
 public class AutoBed extends Module {
@@ -42,24 +35,9 @@ public class AutoBed extends Module {
     private final BooleanSetting rotate = setting("Rotate", false);
     private final BooleanSetting strict = setting("Strict", false);
 
-    private final FloatSetting speed = setting("Speed", 1.0f, 0.1f, 5.0f).invokeTab("Render");
-    private final FloatSetting step = setting("Step", 0.2f, 0.1f, 2.0f).invokeTab("Render");
-    private final FloatSetting opacity = setting("Opacity", 150.0f, 0.0f, 255.0f).invokeTab("Render");
-    private final FloatSetting lineWidth = setting("Line Width", 1.0f, 0.1f, 5.0f).invokeTab("Render");
-    private final ColorSetting color1 = setting("Color 1", new Color(113, 93, 214)).invokeTab("Render");
-    private final ColorSetting color2 = setting("Color 2", new Color(113, 220, 214)).invokeTab("Render");
-
     private final Vec3i[] vec3is = new Vec3i[]{new Vec3i(1, 0, 0), new Vec3i(-1, 0, 0), new Vec3i(0, 0, 1), new Vec3i(0, 0, -1),};
     private AxisAlignedBB bb;
     private long time;
-
-    private final BufferGroup bufferGroup = new BufferGroup(this, z -> true, lineWidth, color1, color2, step, speed, opacity,
-            () -> {
-                if (bb != null) {
-                    RenderUtil.drawBB(bb);
-                }
-            });
-
 
     @EventListener
     public void onMotionUpdate(MotionUpdateEvent event) {
@@ -171,17 +149,6 @@ public class AutoBed extends Module {
             }
         }
     }
-
-    @EventListener
-    public void onRender3D(Render3DEvent event) {
-        GlowShader.render3D(bufferGroup);
-    }
-
-    @EventListener
-    public void onRender2D(Render2DEvent event) {
-        GlowShader.render2D(bufferGroup);
-    }
-
 
     private float rotate(Vec3i vec3i) {
         if (vec3i.getZ() == -1) {
