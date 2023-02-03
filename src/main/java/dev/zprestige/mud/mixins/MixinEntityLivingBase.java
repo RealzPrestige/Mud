bb5cc17331c01cea958c17b93681f6bffa740d77
Vikas Lagerweij <83780520+RealzPrestige@users.noreply.github.com>
2023-02-03 02:43:33 +0100

    ugly code fix

--- src/main/java/dev/zprestige/mud/mixins/MixinEntityLivingBase.java
+++ src/main/java/dev/zprestige/mud/mixins/MixinEntityLivingBase.java
@@ -2,8 +2,11 @@ package dev.zprestige.mud.mixins;
 
 import dev.zprestige.mud.Mud;
 import dev.zprestige.mud.events.impl.world.LastDamageUpdateEvent;
+import dev.zprestige.mud.util.MC;
+import dev.zprestige.mud.util.impl.InventoryUtil;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.init.Items;
 import net.minecraft.util.DamageSource;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
@@ -12,7 +15,7 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(EntityLivingBase.class)
-public abstract class MixinEntityLivingBase extends Entity {
+public abstract class MixinEntityLivingBase extends Entity implements MC {
 
     public MixinEntityLivingBase(World worldIn) {
         super(worldIn);
@@ -20,14 +23,15 @@ public abstract class MixinEntityLivingBase extends Entity {
 
 
     @Inject(method = "attackEntityFrom", at = @At(value = "FIELD", target = "Lnet/minecraft/entity/EntityLivingBase;lastDamage:F", ordinal = 2))
-    private void attackEntityFrom2(DamageSource source, float amount, CallbackInfoReturnable<Boolean> cir){
+    private void attackEntityFrom2(DamageSource source, float amount, CallbackInfoReturnable<Boolean> cir) {
         LastDamageUpdateEvent event = new LastDamageUpdateEvent(this, amount);
         Mud.eventBus.invoke(event);
     }
 
     @Inject(method = "attackEntityFrom", at = @At(value = "FIELD", target = "Lnet/minecraft/entity/EntityLivingBase;lastDamage:F", ordinal = 3))
-    private void attackEntityFrom3(DamageSource source, float amount, CallbackInfoReturnable<Boolean> cir){
+    private void attackEntityFrom3(DamageSource source, float amount, CallbackInfoReturnable<Boolean> cir) {
         LastDamageUpdateEvent event = new LastDamageUpdateEvent(this, amount);
         Mud.eventBus.invoke(event);
     }
 }
+
