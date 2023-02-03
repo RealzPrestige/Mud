ab43d12d9429dfa060be7523b1b73cf028752c33
Vikas Lagerweij <83780520+RealzPrestige@users.noreply.github.com>
2023-02-03 11:18:37 +0100

    forgot this

--- src/main/java/dev/zprestige/mud/module/client/Notifications.java
+++ src/main/java/dev/zprestige/mud/module/client/Notifications.java
@@ -22,6 +22,9 @@ public class Notifications extends Module {
     private final HashMap<String, Integer> popMap = new HashMap<>();
 
     private void onPop(EntityPlayer entityPlayer) {
+        if (entityPlayer.equals(mc.player)) {
+            return;
+        }
 
         int pops = 1;
         if (popMap.containsKey(entityPlayer.getName())) {
